8996f1d46274806b31f49a45ef5d1b320f654920
V David Zvenyach <dave@esq.io>
2020-11-28 14:26:50 -0600

    :sparkles: Automatic PDFs
    With shortcode conversion to blockquotes!

--- Makefile
+++ Makefile
@@ -4,7 +4,11 @@ serve:
 
 pdf:
 		pandoc -s --pdf-engine=xelatex \
+		--lua-filter=shortcodes.lua \
 		-V papersize=a5 \
 		-V documentclass=book \
 		-V header-includes="\usepackage{merriweather}" \
-		./content/chapters/*.md --out test.pdf
+ 		-V header-includes="\usepackage{tcolorbox}" \
+		-V header-includes="\newtcolorbox{myquote}{colback=red!5!white, colframe=red!75!black}" \
+		-V header-includes="\renewenvironment{quote}{\begin{myquote}}{\end{myquote}}" \
+		./content/chapters/*.md --out tech_procurement_book.pdf
---
+++ shortcodes.lua
@@ -0,0 +1,19 @@
+-- Handles hugo-book's `hint` shortcode and converts into a blockquote
+
+incomment = false
+
+function Para(el)
+    if el.content[1].text == "{{<" and el.content[3].text == "hint" and el.content[7].text == ">}}"then
+        incomment = true
+        hint_type = el.content[5].text
+        return pandoc.Para(pandoc.Str())
+    elseif el.content[1].text == "{{<" and el.content[3].text == "/hint" and el.content[5].text == ">}}" then
+        incomment = false
+        return pandoc.Para(pandoc.Str())
+    end
+    if incomment then
+        div = pandoc.Div{pandoc.BlockQuote(pandoc.Para(el.content))}
+        div.classes = {"hint-" .. hint_type}
+        return div
+    end
+end
